17044bd5645a6195c92ee0a02539c6b446537c00
alex george <alexandreantoinegeorge@gmail.com>
2019-02-20 18:22:27 -0500

    tour de manege exercice

--- 06-Cours6_Repeter_des_instructions/la_boucle_for.js
+++ 06-Cours6_Repeter_des_instructions/la_boucle_for.js
@@ -4,3 +4,9 @@ for (compteur = 1; compteur <= 5; compteur++) {
   console.log(compteur);
 }
 console.log("Fin du programme");
+
+// déclaration dans la variable
+for (let i = 1; i <= 5; i++) {
+    console.log(i); // OK
+}
+console.log(i); // Erreur : la variable i n'est pas visible ici
---
+++ 06-Cours6_Repeter_des_instructions/nombre_de_tour.js
@@ -0,0 +1,10 @@
+const nbTours = 10;
+
+console.log("Le manège démarre");
+
+// Ajoutez votre code ici
+for (let tour = 1; tour <= nbTours; tour++) {
+  console.log(`C'est le tour numéro ${tour}`);
+}
+console.log("Le manège s'arrête");
+
