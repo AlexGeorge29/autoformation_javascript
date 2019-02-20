690ca9698ce8ed663422e7a69f360d4ba748854f
alex george <alexandreantoinegeorge@gmail.com>
2019-02-20 18:29:24 -0500

    table de multipliaction exercice

---
+++ 06-Cours6_Repeter_des_instructions/table_de_multiplication.js
@@ -0,0 +1,8 @@
+const nombre = 7; // Faites varier cette variable entre 1 et 10
+
+console.log(`Table de multiplication de ${nombre}`);
+
+// Ajoutez votre code ici
+for (let i = 1; i <= nombre; i++) {
+  console.log(`${i*nombre}`);
+}
