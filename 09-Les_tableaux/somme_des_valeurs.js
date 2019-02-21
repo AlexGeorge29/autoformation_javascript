3591d026aa5256a50d8e3be99869cb7eebb1931b
alex george <alexandreantoinegeorge@gmail.com>
2019-02-20 21:19:38 -0500

    somme des nombres

---
+++ 09-Les_tableaux/somme_des_valeurs.js
@@ -0,0 +1,9 @@
+// calculer sommme du tableau
+
+const nombres = [11, 3, 7, 2, 9, 10];
+let sum = 0;
+// Ajoutez votre code ici
+nombres.forEach(nombre => {
+  return sum += nombre;
+});
+console.log(sum);
