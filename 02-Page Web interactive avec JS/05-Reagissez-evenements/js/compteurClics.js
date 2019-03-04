b4210d4e137e4e74b887235515a4b597fffec415
alex george <alexandreantoinegeorge@gmail.com>
2019-03-04 11:24:13 -0500

    exercice compteur de clisk

--- 02-Page Web interactive avec JS/05-Reagissez-evenements/js/compteurClics.js
+++ 02-Page Web interactive avec JS/05-Reagissez-evenements/js/compteurClics.js
@@ -1,7 +1,16 @@
 const button = document.getElementById("clic");
 const compteur = document.getElementById("compteurClics");
+const desactiver = document.getElementById("desactiver");
 var number = 0;
 button.addEventListener("click", (event) => {
   number += 1
   compteur.innerText = number;
+});
+
+desactiver.addEventListener("click", (event) => {
+  if (button.disabled == true) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
 });
