5371340fa562c82369170dbe027ea536afa5a197
alex george <alexandreantoinegeorge@gmail.com>
2019-03-04 15:36:36 -0500

    cours 6 - formulaires

---
+++ 02-Page Web interactive avec JS/06-Manipuler-les-formulaires/js/cours.js
@@ -0,0 +1,61 @@
+var pseudoElt = document.getElementById("pseudo");
+
+// Affichage d'un message contextuel pour la saisie du pseudo
+pseudoElt.addEventListener("focus", function () {
+    document.getElementById("aidePseudo").textContent = "Entrez votre pseudo";
+});
+// Suppression du message contextuel pour la saisie du pseudo
+pseudoElt.addEventListener("blur", function (e) {
+    document.getElementById("aidePseudo").textContent = "";
+});
+
+// Affichage de la demande de confirmation d'inscription
+document.getElementById("confirmation").addEventListener("change", function (e) {
+    console.log("Demande de confirmation : " + e.target.checked);
+});
+
+// Affichage du type d'abonnement choisi
+var aboElts = document.getElementsByName("abonnement");
+for (var i = 0; i < aboElts.length; i++) {
+    aboElts[i].addEventListener("change", function (e) {
+        console.log("Formule d'abonnement choisie : " + e.target.value);
+    });
+}
+
+// Affichage du code de nationalité choisi
+document.getElementById("nationalite").addEventListener("change", function (e) {
+    console.log("Code de nationalité : " + e.target.value);
+});
+
+var form = document.querySelector("form");
+console.log("Nombre de champs de saisie : " + form.elements.length); // Affiche 10
+console.log(form.elements);
+console.log(form.elements[0].name); // Affiche "pseudo"
+console.log(form.elements.mdp.type); // Affiche "password"
+
+// Vérification de la longueur du mot de passe durant la saisi
+document.getElementById("mdp").addEventListener("input", function (e) {
+    var mdp = e.target.value; // Valeur saisie dans le champ mdp
+    var longueurMdp = "faible";
+    var couleurMsg = "red"; // Longueur faible => couleur rouge
+    if (mdp.length >= 8) {
+        longueurMdp = "suffisante";
+        couleurMsg = "green"; // Longueur suffisante => couleur verte
+    } else if (mdp.length >= 4) {
+        longueurMdp = "moyenne";
+        couleurMsg = "orange"; // Longueur moyenne => couleur orange
+    }
+    var aideMdpElt = document.getElementById("aideMdp");
+    aideMdpElt.textContent = "Longueur : " + longueurMdp; // Texte de l'aide
+    aideMdpElt.style.color = couleurMsg; // Couleur du texte de l'aide
+});
+
+// Contrôle du courriel en fin de saisie
+document.getElementById("courriel").addEventListener("blur", function (e) {
+    var validiteCourriel = "";
+    if (e.target.value.indexOf("@") === -1) {
+        // Le courriel saisi ne contient pas le caractère @
+        validiteCourriel = "Adresse invalide";
+    }
+    document.getElementById("aideCourriel").textContent = validiteCourriel;
+});
