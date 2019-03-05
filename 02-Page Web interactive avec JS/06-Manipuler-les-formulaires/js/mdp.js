39ac297fd759745742ecd36825fa164ffe9ff2f5
alex george <alexandreantoinegeorge@gmail.com>
2019-03-04 21:30:01 -0500

    test mdp

--- 02-Page Web interactive avec JS/06-Manipuler-les-formulaires/js/mdp.js
+++ 02-Page Web interactive avec JS/06-Manipuler-les-formulaires/js/mdp.js
@@ -7,30 +7,33 @@ const info = document.getElementById("infoMdp");
 const message = document.createElement("p");
 info.appendChild(message);
 // Fonction min 6 cractères et cnotient au moisn un chiffre
-function mdpVerification(mdp) {
-  document.getElementById("mdp").addEventListener("input", (event) => {
+// function mdpVerification(mdp) {
+  document.getElementById("mdp1").addEventListener("input", (event) => {
+    var regexMdp = /\d+/;
     if (event.target.value.length < 6) {
-
       message.textContent = "Mot de passe trop court";
-    } else if (event.target.value != [0-9] ) {
+    } else if (!regexMdp.test(mdp1)) {
       message.textContent = "ne contient pas de chiffre";
     }
     else {
       message.textContent = "";
     };
   });
-};
+// };
 // fonction mdp1 et mdp2 sont identiques
 // function sameMdp() {
 
-
+  // mdpVerification(mdp1);
   document.getElementById("mdp1").addEventListener("input", (event) => {
     document.getElementById("mdp2").addEventListener("input", (e) => {
-    console.log(event.target.value)
-    console.log(e.target.value)
+    if (event.target.value != e.target.value) {
+      message.textContent = "Pas le même mot de passe"
+    } else {
+      message.textContent = "";
+    }
     });
   });
 
-// }
+// }!regexMdp.test(mdp1)
 
 // sameMdp();
